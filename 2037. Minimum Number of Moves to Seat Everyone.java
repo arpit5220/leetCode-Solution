a6be0d342b734eed2a646e813e55afe498a4f369
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-04-24 10:34:12 +0530

    Create 2037. Minimum Number of Moves to Seat Everyone.java

---
+++ 2037. Minimum Number of Moves to Seat Everyone.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int sum=0;
+        for(int i=0;i<seats.length;i++)
+        {
+            sum+=Math.abs(seats[i]-students[i]);
+        }
+        
+        return sum;
+    }
+}
+
+// to understand dry run see this video
+// https://www.youtube.com/watch?v=MatKKmBChRY
