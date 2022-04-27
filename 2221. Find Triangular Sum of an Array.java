08d432edfe35e50f870e9f3bb102136c56128cb2
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-04-27 23:18:15 +0530

    Create 2221. Find Triangular Sum of an Array.java

---
+++ 2221. Find Triangular Sum of an Array.java
@@ -0,0 +1,13 @@
+class Solution {
+   public int triangularSum(int[] nums) {
+        int length = nums.length;
+        while(length > 1) {
+            for (int i = 0; i < length -1; i++) {
+                nums[i] = (nums[i] + nums[i+1]) % 10;
+            }
+            length--;
+        }
+        return nums[0];
+    }
+
+}
