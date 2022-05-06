793385e4875d14b4c2e84968fc3e51c7b6941c76
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-05-06 18:33:18 +0530

    Create 561. Array Partition I.java

---
+++ 561. Array Partition I.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int sum=0;
+        for(int i=0;i<nums.length;i=i+2)    /// called greedy
+        {
+            sum+=nums[i];
+        }
+        
+        return sum;
+    }
+}
